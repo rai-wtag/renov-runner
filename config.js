86be2451cd0afaa19d602f9ba4f91233574a14e5
Md Raiyanul Islam <raiyanul.islam@welldev.io>
2026-03-01 12:59:42 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+{
+  "platform": "github",
+  "endpoint": null,
+  "repositories": [
+    "rai-wtag/renov-pilot05",
+    "rai-wtag/renov-pilot06"
+  ]
+}
+// required if we run workflow from a central repo 
